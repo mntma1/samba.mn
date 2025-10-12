6163294964be44c31d737c9f6ffa726405d2f226
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-10-12 02:40:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ DOCKERDIR="/opt/samba"
 SMBCONF="/opt/samba/conf"
 
 # Instlliert den Docker SAMBA Server
-sudo mkdir -p $SDIR/{Data,Backup}
-sudo mkdir -p $SMBCONF
+sudo mkdir -pv $SDIR/{Data,Backup}
+sudo mkdir -pv $SMBCONF
 sudo chown -Rv manfred: $DOCKERDIR
 
 cp docker-compose.yaml install.sh $DOCKERDIR
@@ -20,5 +20,6 @@ And now....
  cd $DOCKERDIR && docker-compose up -d
 
 ========================================
+
 ende
 exit 0
