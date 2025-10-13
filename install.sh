c320f70676f4476e92757d08339471cae8eeae63
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-10-13 10:57:09 +0200

    Update install.sh - Jetzt alles automatisiert :)

--- install.sh
+++ install.sh
@@ -7,20 +7,59 @@ SMBCONF="/opt/samba/conf"
 sudo mkdir -pv $SDIR/{Data,Backup}
 sudo mkdir -pv $SMBCONF
 sudo chown -Rv manfred: $DOCKERDIR
-
 cp -v docker-compose.yaml install.sh $DOCKERDIR
 cp -v smb.conf users.conf $SMBCONF
+
+sleep 3
+
+echo Erforderliche Angaben sind: 
+echo Benutzername, UserID[N] und das Passwort.
+echo ======================================
+read -p "Den Benutzernamen bitte:" SMBUSER
+echo Diese  ID '(ab 1000 und fortlaufend)' wird in SAMBA neu erstellt
+echo und hat mit dem lockalem User nichts zu tun.
+read -p "Die UserID  bitte:" SMBUID
+read -p "Das Passwort bitte:" PASSW
+
+cat<<addsmbuser>>/opt/samba/conf/users.conf
+$SMBUSER:$SMBUID:$SMBUSER:$SMBUID:$PASSW
+addsmbuser
+
+cat<<ende
+
+===================================================
+Der neue SMB-Benutzer ist: $SMBUSER und hat die UID/GID: $SMBUID
+
+Dies ist der neue Eintrag in der: $SMBCONF/users.conf
+
+ $SMBUSER:$SMBUID:$SMBUSER:$SMBUID:$PASSW
+
+ende
+echo ----------------
+sleep 2
+docker-compose -f $DOCKERDIR/docker-compose.yaml up -d
 
 cat<<ende
 
 ========================================
 
-And now fire up....
+ And now fire up....
 
- cd $DOCKERDIR && docker-compose up -d
-oder:
- docker-compose -f $DOCKERDIR/docker-compose.yaml up -d
+  cd $DOCKERDIR && docker-compose up -d
+ oder:
+  docker-compose -f $DOCKERDIR/docker-compose.yaml up -d
+  
 ========================================
 
 ende
+
+cat<<fertig
+
+Im Filemanager eingeben: smb://$USER@HOSTNAME/Data
+
+Na denn -> Viel Apaß
+
+fertig
+sleep 2
+docker logs samba
 exit 0
