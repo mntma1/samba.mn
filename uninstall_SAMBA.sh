9b67b7a3555fe312d36c96609d2b2220c5d42a66
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-10-13 19:28:34 +0200

    Update uninstall_SAMBA.sh

--- uninstall_SAMBA.sh
+++ uninstall_SAMBA.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-docker compose -f /opt/samba/docker-compose.yaml down
-docker image rm c2635b16e76d
+docker compose -f /opt/samba/docker-compose.yaml down 
+docker image rm c2635b16e76d # volumr ID => docker volume ls | grep samba
 sudo rm -rfv /mnt/ssd/ /opt/samba/
 exit 0
 
